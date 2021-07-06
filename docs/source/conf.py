4f89010123ee8eb59fcc0426a0e9cb92303c4df9
zhengY <3289218653@qq.com>
2021-07-06 15:18:21 +0800

    test

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,8 +18,8 @@ sys.path.insert(0, os.path.abspath('./../../code'))
 # -- Project information -----------------------------------------------------
 
 project = 'test'
-copyright = '2021, LZY'
-author = 'LZY'
+copyright = '2021, lzy'
+author = 'lzy'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0'
@@ -30,7 +30,8 @@ release = '1.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc']
+extensions = ['sphinx.ext.autodoc'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -54,6 +55,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
--- docs/source/index.rst
+++ docs/source/index.rst
@@ -1,5 +1,5 @@
 .. test documentation master file, created by
-   sphinx-quickstart on Tue Jul  6 14:05:11 2021.
+   sphinx-quickstart on Tue Jul  6 14:58:17 2021.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
